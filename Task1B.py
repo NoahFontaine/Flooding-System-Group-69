4bf3c3c445ac0296da9e2b20519abea63b497215
ChristineYCLau <123400407+ChristineYCLau@users.noreply.github.com>
2023-01-30 17:31:13 +0000

    Create Task1B.py

---
+++ Task1B.py
@@ -0,0 +1,17 @@
+from floodsystem.stationdata import stations_by_distance
+
+def run():
+    """Requirements for Task 1B"""
+    x = stations_by_distance()
+    print( x[:10])
+    
+    #build a list of tuples (station name, town, distance)
+    #list2 = staions_by_distance()
+    #list3 = []
+    #for station in list2:
+     # list3.append[tuple(station.name, station.town, distance)]
+    #return list3
+      
+   
+
+
