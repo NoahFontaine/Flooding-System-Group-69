ba7bf6ce11f88922282bec5877911c929ddea9a2
ChristineYCLau <123400407+ChristineYCLau@users.noreply.github.com>
2023-01-30 17:27:35 +0000

    Update geo.py

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,17 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+
+
+from haversine import haversine, Unit
+from floodsystem.stationdata import build_station_list
+
+def stations_by_distance(stations, p):
+  list1 = []
+  for station in staions:
+    distance = haversine(station.coord, p)
+    list1.append[tuple(station, distance)]
+  list1.sorted_by_key
+  return list1
+  
+  
