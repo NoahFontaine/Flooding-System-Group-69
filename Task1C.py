bb11aaa75bbda469f102265087f1fc03ff56bc6b
Noah Fontaine <nf391@cam.ac.uk>
2023-02-02 20:37:49 +0000

    Completed Task 1C program

--- Task1C.py
+++ Task1C.py
@@ -1,2 +1,9 @@
 """"Demonstration program for Task 1C"""
 
+from floodsystem.geo import stations_within_radius
+from floodsystem.stationdata import build_station_list
+from floodsystem.utils import sorted_by_key  # noqa
+
+
+# This program prints the list returned by "stations_within_radius", sorted using the "sorted_by_key" on first entry
+print(sorted_by_key(stations_within_radius(build_station_list(), (52.2053, 0.1218), 10), 0))
