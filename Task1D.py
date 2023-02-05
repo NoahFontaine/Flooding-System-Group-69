4defac7f45b00008d29e4264db1d03cc02de5b02
ChristineYCLau <123400407+ChristineYCLau@users.noreply.github.com>
2023-02-05 16:49:05 +0000

    Update Task1D.py

--- Task1D.py
+++ Task1D.py
@@ -5,17 +5,23 @@ from floodsystem.stationdata import build_station_list
 if __name__ == "__main__":
   print("*** Task 1D: CUED Part IA Flood Warning System ***")
 
-  x = rivers_with_station(build_station_list()) 
-  print(len(x)) # number of rivers with at least one moniroting system
+  s1 = rivers_with_station(build_station_list()) 
+  print(len(s1)) # number of rivers with at least one moniroting system
+  x = list(s1)
   sorted(x)
   print(x[:10]) # first 10 rivers in alphabetical order
 
   # This function prints the names of stations located on the following rivers in alphabetical order
   y = stations_by_river(build_station_list())
-  sorted(y)
-  print(y['River Aire']) 
-  print(y['River Cam'])
-  print(y['River Thames'])
+  l1 = y['River Aire']
+  l1.sort
+  print(l1) # stations located on River Aire
+  l2 = y['River Cam']
+  l2.sort
+  print(l2) # stations located on River Cam
+  l3 = y['River Thames']
+  l3.sort
+  print(l3) # stations located on River Thames
 
 
 
