50d5be22ca33fe816c3e95e9514395cf9e27ad27
ChristineYCLau <123400407+ChristineYCLau@users.noreply.github.com>
2023-02-05 16:04:17 +0000

    Add demonstration program for task1d

--- Task1D.py
+++ Task1D.py
@@ -4,3 +4,16 @@ from floodsystem.stationdata import build_station_list
 
 if __name__ == "__main__":
   print("*** Task 1D: CUED Part IA Flood Warning System ***")
+  
+
+x = rivers_with_station(build_station_list()) 
+print(len(x)) # number of rivers with at least one moniroting system
+sorted(x)
+print(x[:10]) # first 10 rivers in alphabetical order
+
+y = stations_by_river(build_station_list())
+print(y['River Aire'])
+print(y['River Cam'])
+print(y['River Thames'])
+
+
