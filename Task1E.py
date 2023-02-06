4caae33ce7953e2093b02069f1ce4dab3bac4235
ChristineYCLau <123400407+ChristineYCLau@users.noreply.github.com>
2023-02-06 01:02:17 +0000

    Update Task1E.py

--- Task1E.py
+++ Task1E.py
@@ -2,4 +2,5 @@ from floodsystem.geo import rivers_by_station_number, rivers_with_station, stati
 from floodsystem.stationdata import build_station_list
 
 if __name__ == "__main__":
+    print("*** Task 1E: CUED Part IA Flood Warning System ***")
     print(rivers_by_station_number(build_station_list(), 9))
