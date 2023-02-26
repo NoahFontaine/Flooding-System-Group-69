3679886176c46551d954e7d401b2761065be84ca
Noah Fontaine <nf391@cam.ac.uk>
2023-02-26 23:38:13 +0000

    Added comments to Task 2C

--- Task2C.py
+++ Task2C.py
@@ -3,13 +3,14 @@ from floodsystem.flood import stations_level_over_threshold, stations_highest_re
 
 def run():
     stations = build_station_list()
-    update_water_levels(stations)
-    x = stations_highest_rel_level(stations, 10)
-    y = stations_level_over_threshold(stations, 0)
+    update_water_levels(stations) # Updates the water level
+    x = stations_highest_rel_level(stations, 10) # Computes the first 10 station names with the highest level
+    y = stations_level_over_threshold(stations, 0) # Computes all the relative water levels
     list = []
     for i in range(len(x)):
-        list.append((x[i], y[i][1]))
+        list.append((x[i], y[i][1])) # Creates a list with the first 10 station names and the matched water level
     for i in list:
+        # Prints in the correct format
         print(str(i[0]), str(i[1]))
 
 
