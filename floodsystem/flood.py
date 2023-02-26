30956e6c8f8adef62ed439f8ccaf993515e3cd9f
Noah Fontaine <nf391@cam.ac.uk>
2023-02-26 23:34:05 +0000

    Added comments to flood

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -6,13 +6,16 @@ def stations_level_over_threshold(stations, tol):
     for station in stations:
         if station.relative_water_level() != None:
             relative = station.relative_water_level()
+            # If relative is greater than tolerance, adds that station to the list
             if relative > tol:
                 list.append((station.name, station.relative_water_level()))
+    # Returns sorted list
     return sorted_by_key(list, 1, reverse=True)
 
 def stations_highest_rel_level(stations, N):
     list = stations_level_over_threshold(stations, 0)
     list1 = []
+    # Adds the relative water level to the list and returns the first N terms
     for i in range(N):
         list1.append(list[i][0])
     return list1
