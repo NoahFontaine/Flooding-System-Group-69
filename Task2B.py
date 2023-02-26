b94b6f10b1a42b9408cdae9bf4de5945e0aab106
Noah Fontaine <nf391@cam.ac.uk>
2023-02-26 23:36:23 +0000

    Added comments to Task2B

--- Task2B.py
+++ Task2B.py
@@ -5,11 +5,12 @@ from floodsystem.flood import stations_level_over_threshold
 def f():
     stations = build_station_list()
     update_water_levels(stations)
+    # Computes all the stations and the relative water level that have a relative level above 0.8
     x = stations_level_over_threshold(stations, 0.8)
     for i in x:
+        # Prints that list in the required way
         print(str(i[0]), str(i[1]))
 
-#print(stations_level_over_threshold(build_station_list(), 0.9))
 if __name__ == "__main__":
     print("*** Task 2B: CUED Part IA Flood Warning System ***")
     f()
