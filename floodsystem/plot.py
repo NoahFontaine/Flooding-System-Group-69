256fcdffba67a394e50c06a19b7ab6d9d68d3c40
ChristineYCLau <123400407+ChristineYCLau@users.noreply.github.com>
2023-02-27 16:52:56 +0000

    Attempted task 2e

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -1,1 +1,16 @@
+import matplotlib.pyplot as plt
+from datetime import datetime, timedelta
+
+def plot_water_levels(station, dates, levels):
+    
+    plt.plot(dates, levels)
+
+    plt.xlabel('date')
+    plt.ylabel('water level(m)')
+    plt.xticks(rotation=45)
+    plt.title(station.name)
+
+    plt.tight_layout
+
+    plt.show
 
