198bc6b38c781f44a8f45c823eb7a856ba672b4e
ChristineYCLau <123400407+ChristineYCLau@users.noreply.github.com>
2023-02-27 18:30:37 +0000

    trying on task 2e
    still working on it

--- Task2E.py
+++ Task2E.py
@@ -1,3 +1,25 @@
+import datetime
+from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.flood import stations_highest_rel_level
+from floodsystem.plot import plot_water_levels
+from floodsystem.datafetcher import fetch_measure_levels
+
+def run():
+    stations = build_station_list()
+    update_water_levels(stations)
+    list = stations_highest_rel_level(stations, 5)
+    print (list)
+    list2 = []
+    dt = 10
+    for station in stations:
+        if station.name in list:
+            dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
+            plot_water_levels(station, dates, levels)
+           
+        else:
+            pass
+
+
 if __name__ == "__main__":
     print("*** Task 2E: CUED Part IA Flood Warning System ***")
     run()
