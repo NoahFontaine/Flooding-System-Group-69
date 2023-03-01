13a88a490e6370ab99ebeeca09f24318962df5d2
Noah Fontaine <nf391@cam.ac.uk>
2023-03-01 11:31:46 +0000

    Trying to fix issue in test_analysis

--- test_analysis.py
+++ test_analysis.py
@@ -1,8 +1,6 @@
 from floodsystem.analysis import polyfit
-from floodsystem.stationdata import MonitoringStation
 import datetime
 from floodsystem.stationdata import build_station_list, update_water_levels
-from floodsystem.plot import plot_water_levels
 from floodsystem.datafetcher import fetch_measure_levels
 from floodsystem.flood import stations_highest_rel_level
 
@@ -15,7 +13,7 @@ def test_polyfit():
     update_water_levels(stations)
     list = stations_highest_rel_level(stations, 1)
    
-    dt = 10
+    dt = 2
     for station in stations:
         if station.name in list:
             dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
