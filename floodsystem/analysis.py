9562a9ed5f1ad441a266b8b8d4cbba64381f3bc8
ChristineYCLau <123400407+ChristineYCLau@users.noreply.github.com>
2023-03-01 04:28:09 +0000

    done task 2g

--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib
 
+from datetime import datetime, timedelta
+
 def polyfit(dates, levels, p):
   x = matplotlib.dates.date2num(dates) # List of all the dates converted as integers
   # Calculates the best fit coefficients with a shift in the x-values of value x[0] to avoid any errors
@@ -9,4 +11,34 @@ def polyfit(dates, levels, p):
   poly = np.poly1d(a_n)
 
   return poly, x[0]
+
+def trend(station, dates, levels, p=4):
+
+    a_n, d0 = polyfit(dates, levels, p)
+    x = matplotlib.dates.date2num(dates)
+    x_axis = np.linspace(x[0], x[-1], 50)
+    
+    y = (a_n(x_axis - d0))
+   
+    low = station.typical_range[0]
+    high = station.typical_range[1]
+  
+    slope = np.diff(y)
+
+  # criteria 1. max value + 2. rising or falling trend
+
+    if max(y) >= high and slope[-1]>0:
+      return(station.town,"severe")
+  
+    elif max(y) >=high and slope[-1]<=0:
+      return(station.town, "high")
+    
+    elif high>max(y)>= low:
+      return(station.town, "moderate")
+    
+    elif max(y)<low:
+      return(station.town,"low")
+  
+  
+
 
